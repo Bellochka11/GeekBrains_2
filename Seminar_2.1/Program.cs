d1be29e958a424ea9cad86c692d27dc1cb03ea3e
Белянкина Елизавета <Belyankina@mail.ru>
2023-02-15 20:54:29 +0300

    координаты точки (X и Y) номер четверти

---
+++ Seminar_2.1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти 
+// плоскости, в которой находится эта точка.
+
+Console.WriteLine("Введите X");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите Y");
+int y = int.Parse(Console.ReadLine()!);
+if (y > 0)
+{
+    if(x > 0)
+    {
+       Console.WriteLine("Первая четверть");
+    }
+    if(x < 0)
+    { 
+        Console.WriteLine("Вторая четверть");
+    }
+}
+if (y < 0)
+{
+    if(x > 0)
+    {
+       Console.WriteLine("Четвертая четверть");
+    }
+    if(x < 0)
+    { 
+        Console.WriteLine("Третья четверть");
+    }
+}
+
+
