dadb2600e32d77a40c9789fd662a74e26501fae8
Белянкина Елизавета <Belyankina@mail.ru>
2023-02-15 23:42:50 +0300

    число (N) таблица квадратов чисел от 1 до N

---
+++ Seminar_2.4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25. 2 -> 1,4
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int count = 1;                           //счетчик
+while (count <= number)
+{
+   double result = Math.Pow(count,2);
+   Console.Write($"{result} ");
+   count++;
+}
