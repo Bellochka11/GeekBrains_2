0e1185989c811db8826e43d6c8d5004457a8cbf6
Белянкина Елизавета <Belyankina@mail.ru>
2023-02-15 19:14:46 +0300

    вход два числа и одно число квадратом другого

---
+++ Seminar_1.5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+//● 5, 25 -> да -4, 16 -> да 25, 5 -> да 8,9 -> нет
+Console.WriteLine("Введите два числа: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+if (a * a == b || b * b == a)
+{
+    Console.WriteLine("Да");
+}
+else
+Console.WriteLine("Нет");
